ab7d6944c1d3c923471fe68568000fc414d00ffb
Vu Huy Minh Pham <vuhp2@uci.edu>
2026-02-22 19:10:59 -0800

    add: an idea of a program state class

---
+++ grin/execution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class ProgramState:
+    """
+    Keeps track of the program's state
+    - token_lines is the program itself in grin tokens
+    - ip is where the execution currently is
+    - vars stores variables
+    - goto_labels is a dictionary that should enable the goto functionality to work.
+    - return_stack is list designed like a stack to keep track of return values
+    - output stores list of values being printed
+    """
+    def __init__(self, token_lines):
+        self.token_lines = token_lines
+        self.ip = 0
+        self.vars = {}
+        self.goto_labels = {}
+        self.return_stack = []
+        self.output = []
